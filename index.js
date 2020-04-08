c17e346e3d6f83aadf2c3e9aa44ac313045c5035
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-04-08 12:05:57 -0300

    return to old version

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import express from "express";
-import cors from "cors";
-import { json } from "body-parser";
-import Constants, { PORT } from "./utils/Constants";
-import routes from "./routes";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Constants = require("./utils/Constants");
+const routes = require("./routes");
 const MongoClient = new (require("./clients/MongoClient"))();
 // const { addRouteEndpoint } = require("./utils/RouteUtils");
 // const mongo = require("mongodb");
 
 const app = express();
 
-app.use(json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use("/mock-server", routes);
 
-app.listen(PORT, (err) => {
+app.listen(Constants.PORT, (err) => {
   if (err) {
     console.error(err);
     return;
@@ -25,5 +25,5 @@ app.listen(PORT, (err) => {
     console.error(err);
   });
 
-  console.log(`Mock server app listening on port ${PORT}!`);
+  console.log(`Mock server app listening on port ${Constants.PORT}!`);
 });
